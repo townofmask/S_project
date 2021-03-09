c11179dad216ba2f34936837a1aa460c1c8a6e12
Juliya <73654381+townofmask@users.noreply.github.com>
2021-03-10 01:02:36 +0500

    Start

---
+++ S_Project/S_Project.py
@@ -0,0 +1,39 @@
+import time
+import openpyxl
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+output = []
+# time.sleep(5)
+
+driver.get("https://www.kamogo.com/7")
+# time.sleep(5)
+
+textarea = driver.find_element_by_css_selector(".row>.form-control")
+content_text = driver.find_element_by_css_selector("div#content-text>p")
+text = content_text.text
+
+book = openpyxl.Workbook()
+sheet = book.active
+sheet["A1"] = text 
+
+i = 17
+textarea.send_keys(i)
+res = driver.find_element_by_id("result")
+output = res.text
+sheet["A2"] = str(i)
+sheet["B2"] = ">>>"
+sheet["C2"] = output
+
+for i in range(101):
+    textarea.send_keys(i)
+    res = driver.find_element_by_id("result")
+    output = res.text
+    # print(str(i) + ">>>"+ output + "\n")
+    # time.sleep(1)
+    driver.find_element_by_css_selector(".row>.form-control").clear()
+
+book.save("parse.xlsx")
+book.close()
+
+driver.quit()
